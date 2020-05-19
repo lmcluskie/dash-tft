a1c063d70599d8f8981f6edab586ba0963eca7b4
mcluskLuke <45476085+mcluskLuke@users.noreply.github.com>
2020-05-20 02:12:05 +1200

    Removed patch comparison

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import dash
 import dash_table
 import dash_core_components as dcc
 import dash_html_components as html
-import dash_daq as daq
 from dash.dependencies import Input, Output
 import numpy as np
 import pandas as pd
@@ -19,28 +18,21 @@ colors = {
 line_colors = ['#47D0E8', '#EF9A45', '#8DF279', '#006DDB', '#D16C00', '#477A3D']
 static_columns = ['Level', 'Tier', 'Copies Wanted']
 var_columns = ['Champ Copies Owned', 'Tier Copies Owned']
-patch_current = '10.9'
-patch_compare = '9.21'
+patch_current = '10.10'
 levels = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 tiers = [1, 2, 3, 4, 5]
 
 uniques = {
-    patch_current: [12, 12, 12, 9, 7],
-    patch_compare: [13, 13, 13, 10, 8]
+    patch_current: [12, 12, 12, 9, 7]
 }
 copies = {
-    patch_current: [29, 22, 16, 12, 10],
-    patch_compare: [39, 26, 18, 13, 10]
+    patch_current: [29, 22, 16, 12, 10]
 }
 weights = {
     patch_current:
         [[100, 0, 0, 0, 0], [100, 0, 0, 0, 0], [75, 25, 0, 0, 0],
          [60, 30, 10, 0, 0], [40, 35, 20, 5, 0], [25, 35, 30, 10, 0],
          [19, 30, 35, 15, 1], [14, 20, 35, 25, 6], [10, 15, 25, 35, 15]],
-    patch_compare:
-        [[100, 0, 0, 0, 0], [100, 0, 0, 0, 0], [70, 25, 5, 0, 0],
-         [50, 35, 15, 0, 0], [35, 35, 25, 5, 0], [25, 35, 30, 10, 0],
-         [20, 30, 33, 15, 2], [15, 20, 35, 22, 8], [10, 15, 30, 30, 15]]
 }
 
 
@@ -62,10 +54,10 @@ def calculate_final_state(level, tier, goal, c_owned, t_owned, rolls, patch):
             m[i, i] = 1 - prob
             m[i, i + 1] = prob
         m[goal, goal] = 1
-        # full transition matrix
-        full_transition_matrix = np.linalg.matrix_power(m, rolls * 5)
+        # full transition matrix on each roll
+        roll_transition_matrix = np.linalg.matrix_power(m, rolls * 5)
         # final state vector based on inital state and transition matrix
-        final = np.matmul(start, full_transition_matrix)
+        final = np.matmul(start, roll_transition_matrix)
         return final[0, -1]
     except TypeError:
         return 0
@@ -86,7 +78,7 @@ def iterate_calculations(df, patch):
                     if prob >= 0.5:
                         medians[f'{scenario + 1}'] = i
                     elif i == 100:
-                        medians[f'{scenario + 1}'] = f'{round(prob*100, 2)}%'
+                        medians[f'{scenario + 1}'] = '>100'
         except IndexError:
             probabilities[f'{scenario + 1}'] = [0] * 100
     return probabilities, medians
@@ -191,25 +183,8 @@ app.layout = html.Div([
             }
         ),
 
-        html.Div([
-                daq.ToggleSwitch(
-                    id='comparison-toggle',
-                    label=f'Compare to Patch {patch_compare} (Set 1)',
-                    labelPosition='top',
-                    color=colors['cells'],
-                    value=False
-                )
-            ],
-            style={
-                'fontFamily': 'Garamond',
-                'width': '60%',
-                'display': 'inline-block',
-                'color': colors['text']
-            }
-        ),
-
         html.H6(
-            'Median Rolls Required (Chance of success after 100 rolls if median > 100)',
+            'Median Rolls Required',
             style={
                 'fontFamily': 'Bodoni',
                 'textAlign': 'center',
@@ -255,18 +230,13 @@ app.layout = html.Div([
     [Output('search-graph', 'figure'),
      Output('median-table', 'children')],
     [Input('search-input-table', 'data'),
-     Input('search-input-table', 'columns'),
-     Input('comparison-toggle', 'value')]
+     Input('search-input-table', 'columns')]
 )
-def update_graph(rows, columns, compare):
+def update_graph(rows, columns):
     df = pd.DataFrame(rows, columns=[c['name'] for c in columns])
     lines = {}
     results = {}
     lines[patch_current], results[patch_current] = iterate_calculations(df, patch_current)
-    if compare:
-        lines[patch_compare], results[patch_compare] = iterate_calculations(df, patch_compare)
-    else:
-        lines[patch_compare], results[patch_compare] = {'1': [0]*101, '2': [0]*101}, {'1': '-', '2': '-'}
     graph = {
         'data': [
             go.Scatter(
@@ -286,31 +256,11 @@ def update_graph(rows, columns, compare):
                     'color': line_colors[1]
                 },
                 name='B'
-            ),
-            go.Scatter(
-                x=list(range(101)),
-                y=lines[patch_compare]['1'],
-                mode='lines',
-                line={
-                    'color': line_colors[3]
-                },
-                name=f'{patch_compare} A',
-                visible=compare
-            ),
-            go.Scatter(
-                x=list(range(101)),
-                y=lines[patch_compare]['2'],
-                mode='lines',
-                line={
-                    'color': line_colors[4]
-                },
-                name=f'{patch_compare} B',
-                visible=compare
-            ),
+            )
         ],
         'layout': go.Layout(
             title=(
-                f'Prob(Units Found) vs Rolls '
+                f'Prob(Units Found) vs Rolls'
             ),
             titlefont={
                 'color': colors['title'],
@@ -328,7 +278,7 @@ def update_graph(rows, columns, compare):
                 'fixedrange': True
             },
             yaxis={
-                'title': 'Probability all units found (%)',
+                'title': 'Probability all units found',
                 'showline': True,
                 'linewidth': 2,
                 'linecolor': colors['text'],
@@ -359,7 +309,7 @@ def update_graph(rows, columns, compare):
             paper_bgcolor=colors['paper']
         )
     }
-    columns = ['Patch↓ Scenario→', 'A', 'B']
+    columns = ['Scenario', 'Rolls']
     table = dash_table.DataTable(
         columns=[
             {'name': f'{i}',
@@ -369,15 +319,13 @@ def update_graph(rows, columns, compare):
         ],
         data=[
             {
-                'Patch↓ Scenario→': patch_current,
-                'A': results[patch_current]['1'],
-                'B': results[patch_current]['2']
+                'Scenario': 'A',
+                'Rolls': results[patch_current]['1']
             },
             {
-                'Patch↓ Scenario→': patch_compare,
-                'A': results[patch_compare]['1'],
-                'B': results[patch_compare]['2']
-            },
+                'Scenario': 'B',
+                'Rolls': results[patch_current]['2']
+            }
         ],
         editable=False,
         style_header={
@@ -389,10 +337,6 @@ def update_graph(rows, columns, compare):
             'color': colors['text'],
             'textAlign': 'center'
         },
-        style_cell_conditional=[
-            {'if': {'column_id': 'Patch↓ Scenario→'},
-             'width': '40%'}
-        ],
         css=[
             {'selector': 'td.cell--selected, td.focused', 'rule': 'background-color: #192A35 !important;'},
             {'selector': 'td.cell--selected *, td.focused *', 'rule': 'color: #FFFFFF !important;'}
